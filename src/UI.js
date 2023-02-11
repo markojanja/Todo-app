9ea5159e3d6e169e2cfc2aec41b7bc0ce9b4e3e2
Marko Janjic <marko.m.janjic@gmail.com>
2023-02-11 10:47:51 +0100

    fixed project rendering on btn clicks

--- src/UI.js
+++ src/UI.js
@@ -2,43 +2,62 @@ import ProjectList from "./ProjectsList";
 
 export default class UI {
   static loadUI() {
-    UI.createNewProject();
+    UI.createProjectController();
     UI.renderProjectList();
     UI.setActiveProject();
-    UI.UpdateProject();
+    UI.UpdateProjectController();
+    UI.toggleFormButtonController();
   }
   static projects = new ProjectList();
 
-  static createNewProject() {
+  static toggleFormButtonController() {
+    const toggleBtn = document.querySelector(".toggle-form");
+    toggleBtn.addEventListener("click", () => {
+      const form = document.getElementById("myForm");
+      form.classList.toggle("show");
+    });
+  }
+
+  static createProjectController() {
     const myForm = document.getElementById("myForm");
     myForm.addEventListener(
       "submit",
       (e) => {
         e.preventDefault();
-        const title = document.getElementById("taskName");
-        UI.projects.addProjects(title.value);
+        const taskName = document.getElementById("taskName");
+        if (!taskName.value) return console.log("this is empty");
+        UI.projects.addProjects(taskName.value);
+        const p = UI.projects.selectProject(taskName.value);
+        UI.renderProject(p);
         this.renderProjectList();
         myForm.reset();
+        myForm.classList.remove("show");
       },
       false
     );
   }
   // Update project
-  static UpdateProject() {
-    const cards = document.querySelectorAll(".project-card");
+  static UpdateProjectController() {
+    const cards = document.querySelectorAll(".project-link");
     cards.forEach((card) => {
-      const btn = card.querySelector(".btn-edit");
-      const modal = card.querySelector(".modal");
+      const editButton = card.querySelector(".btn-edit");
+      editButton.addEventListener("click", (e) => {
+        const modal = card.querySelector(".modal");
+        let projectName = e.target.dataset.value;
 
-      btn.addEventListener("click", (e) => {
-        modal.style.display = "block";
-        const projectName = e.target.dataset.value;
+        modal.style.display = "flex";
+
+        UI.renderProject(UI.projects.selectProject(projectName))
         modal.addEventListener("submit", (e) => {
           e.preventDefault();
-          const input = document.querySelector(`#input-${projectName}`);
-          console.log(input.value);
+          const input = document.getElementById(`input-${projectName}`);
+          if (!input.value) return console.log("this is empty");
           this.projects.updateProjects(projectName, input.value);
           this.renderProjectList();
+          if (projectName) {
+            projectName = input.value;
+            UI.renderProject(UI.projects.selectProject(projectName));
+          }
           modal.reset();
         });
       });
@@ -47,19 +66,18 @@ export default class UI {
 
   //Delete project
 
-  static deleteProject() {
-    const deleteButton = document.querySelectorAll(".btn-delete");
+  static deleteProjectController() {
+    const deleteButtons = document.querySelectorAll(".btn-delete");
     const tasks = document.querySelector("#project");
-    this.setActiveProject()
+    this.setActiveProject();
 
-    deleteButton.forEach((bnt) => {
-      bnt.addEventListener("click", (e) => {
+    deleteButtons.forEach((button) => {
+      button.addEventListener("click", (e) => {
         const projectName = e.target.dataset.value;
-        UI.projects.projectList.forEach(proj=>{
-          if (proj!==projectName) tasks.innerHTML = ""
-          this.renderProjectList()
-        })
-        // console.log(UI.projects.projectList)
+        UI.projects.projectList.forEach((proj) => {
+          if (proj !== projectName) tasks.innerHTML = "Project Deleted";
+          this.renderProjectList();
+        });
         UI.projects.deleteProjects(projectName);
         this.renderProjectList();
       });
@@ -71,24 +89,23 @@ export default class UI {
     list.innerHTML = "";
     UI.projects.projectList.map((project) => {
       list.innerHTML += `
-      <div class="project-card">
+      <li class="project-link">
       <h3 class="title">${project.name}</h3>
       <button class="btn-edit" type="button" data-value="${project.name}">Edit</button>
       <button class="btn-delete" type="button" data-value="${project.name}">delete</button>
-        <form class="modal" style="display: none;">
-           <input type="text" class="inputVal" id="input-${project.name}">
+        <form class="modal">
+           <input type="text" class="inputVal" id="input-${project.name}" required>
           <button type="submit" id="btn3">Update</button>
          </form>
-      </div>
+      </li>
       `;
     });
-    this.UpdateProject();
-    this.deleteProject();
+    this.UpdateProjectController();
+    this.deleteProjectController();
   }
 
   static setActiveProject() {
     const projectsNames = document.querySelectorAll(".title");
-    // console.log("hiiii");
     projectsNames.forEach((name) => {
       name.addEventListener(
         "click",
@@ -104,14 +121,14 @@ export default class UI {
 
   static renderProject(project) {
     const tasks = document.querySelector("#project");
-    tasks.innerHTML=""
-    tasks.innerHTML = `<h1 class= 'pt'>${project.name}</h1>`;
+    tasks.innerHTML = "";
+    tasks.innerHTML = `<h1 class='pt'>${project.name}</h1>`;
     tasks.innerHTML += `
     <form action="" id="myForm2">
-      <input type="text" name="project" id="taskName2">
+      <input type="text" name="project" id="taskName2" required>
       <button type="submit" id="btn">Add task</button>
     </form>
-    <div id="taskList"></div>`;
+    <ul id="taskList"></ul>`;
 
     UI.taskFormHandler();
     if (project.todos.length > 0) {
@@ -137,7 +154,7 @@ export default class UI {
         pr.addTodo(title.value);
         tasks2.innerHTML = "";
         pr.todos.map((todo) => {
-          tasks2.innerHTML += `<p>${todo}</p>`;
+          tasks2.innerHTML += `<li>${todo}</li>`;
         });
         myForm2.reset();
       },
